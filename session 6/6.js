c0157a0077a3c827c9ae6586de9500c2a61f8ecb
Ali <sabzinezhad93@gmail.com>
2023-09-10 16:26:34 +0200

    session 6

---
+++ session 6/6.js
@@ -0,0 +1,12 @@
+// npm package
+// npm: node package manager
+
+//
+// SPA, MPA
+// Single page application
+// Multi page application
+
+//
+// CSR - Client side rendering // react js - vue js
+// SSR - Server side rendering // next js - nuxt js
+// SSG - Static site generation // next js
